a89e22ee40b0543382a9e61c6e9ba230d6be6736
reasat-rafio <59998970+reasat-rafio@users.noreply.github.com>
2020-08-28 02:18:20 +0600

    react part only

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+import LearningReduce from "./components/LearningReduce";
+import { GlobalProvider } from "./components/context/GlobalState";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+        <LearningReduce />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
