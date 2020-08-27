ddcff1b196f7aaeb47e4beadf405892c0a443e41
reasat-rafio <59998970+reasat-rafio@users.noreply.github.com>
2020-08-28 01:17:02 +0600

    only react part

--- src/components/context/AppReducer.js
+++ src/components/context/AppReducer.js
@@ -3,6 +3,14 @@ export default (state, action) => {
     case "DELETE_TRANSACTION":
       return {
         ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
       };
     default:
       return state;
